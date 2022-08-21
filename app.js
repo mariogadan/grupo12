bcdf86b2847de7626807ba82188fa195801ffdf4
Damian Palla <damian.palla@live.com>
2022-08-21 17:16:12 -0300

    m

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ app.use(express.static(path.join(__dirname , './public')))
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname , './views/home.html'))
 })
+
+app.get('/producto', function(req, res){
+    res.sendFile(path.join(__dirname , './views/producto.html'))
+})
+
+app.get('/carrito', function(req, res){
+    res.sendFile(path.join(__dirname , './views/carrito.html'))
+})
+
+app.get('/login', function(req, res){
+    res.sendFile(path.join(__dirname , './views/login.html'))
+})
+
+app.get('/registro', function(req, res){
+    res.sendFile(path.join(__dirname , './views/registro.html'))
+})

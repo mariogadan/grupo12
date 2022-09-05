261d7863af5464d559f1ab3944162a82f2e273db
mariogadan <mario.gadan@gmail.com>
2022-09-05 17:46:31 -0300

    Modifico archivos para Heroku

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+
+app.use(express.static(path.join(__dirname , './public')))
+
 app.listen(process.env.PORT || 3030, function(){
     console.log('Servidor activo')
 })
-
-app.use(express.static(path.join(__dirname , './public')))
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname , './views/home.html'))

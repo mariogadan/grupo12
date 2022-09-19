edbd683cc9d77657cbbea3cba7e838b89fd625bf
OgrizovicFlavio <flavio.ogrizovic@gmail.com>
2022-09-19 19:00:34 -0300

    Corrigiendo rutas

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const port = process.env.PORT || 3030
 const mainRoutes = require("./src/routes/main.routes");
 const usuarioRoutes = require("./src/routes/usuario.routes");
 
+app.use(express.static(path.join(__dirname , './public')))
+
 app.set('view engine', 'ejs');
 
-app.set("views", "./src/views")
+app.set("views", "./src/views");
 
 app.listen(port, function(){
     console.log('Servidor activo')
-})
-
-app.use(express.static(path.join(__dirname , './public')))
+});
 
 app.use("/", mainRoutes);
 

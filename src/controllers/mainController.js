39d3f1c34b87342032c85539f26574d6addb4af4
delfivilieri <110919394+delfivilieri@users.noreply.github.com>
2022-09-30 18:14:51 -0300

    .

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,6 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../database/cursosDataBase.json');
+const products =JSON.parse(fs.readFileSync(productsFilePnpath,"utf-8"));
+
+
+
 const controlador = {
     home: function (req, res) {
-    res.render("home")
+    res.render("home", {productos:products});
 },
 
     producto: function (req, res) {

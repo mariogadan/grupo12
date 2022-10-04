6b514bad624cb1427fae0896efd21534221c225b
macarenazapp <zappemac@gmail.com>
2022-10-03 23:12:54 -0300

    .

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -2,9 +2,8 @@ const { application } = require('express');
 const express = require('express');
 const router = express.Router();
 const controlador = require("../controllers/mainController");
-
+router.get("/beta", controlador.beta);
 router.get("/", controlador.home);
-
 router.get("/crear", controlador.crear);
 router.post("/crear", controlador.crearCurso);
 
@@ -15,7 +14,7 @@ router.get("/editar/:id", controlador.editar);
 router.put("/editar/:id", controlador.editarCurso);
 
 router.get("/carrito", controlador.carrito);
-router.get("/beta", controlador.beta);
+
 
 module.exports = router
 

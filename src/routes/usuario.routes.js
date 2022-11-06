f0eb1542ca2a71eec46e404cfaa3079e1546f008
OgrizovicFlavio <flavio.ogrizovic@gmail.com>
2022-11-06 17:52:02 -0300

    Resolviendo conflictos

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -5,21 +5,10 @@ const controlador = require("../controllers/usuarioController");
 const subirImagenRegistro = require('../middlewares/imgRegistroMid');
 const validacionRegistro = require('../middlewares/validacionRegistroMid')
 
-
-const validacionRegistro = [
-    body('nombre').notEmpty().withMessage('Completar nombre'),
-    body('apellido').notEmpty().withMessage('Completar apellido'),
-    body('nombreUsuario').notEmpty().withMessage('Completar nombre de usuario'),
-    body('email')
-        .notEmpty().withMessage('Completar email').bail()   // el bail() detiene las validaciones siguientes en caso de que no se cumpla la que tiene el bail(). Se usa  para hacer mas de una validacion,  
-        .isEmail().withMessage('El formato del correo no es válido')  // en este caso si ya el campo esta vacio, no sigue con las validaciones siguientes, y si no esta vacio, valida que sea un mail (para este caso particular)
-];
-
-
 // Login
 
 router.get("/login", controlador.login) ;
-router.post("/login" , controlador.procesoLogin) ;
+router.post("/login", controlador.procesoLogin) ;
 
 // Formulario de registro
 

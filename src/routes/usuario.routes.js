fd39ba81a3f10b34ed3ce063d482f633a588a410
delfivilieri <110919394+delfivilieri@users.noreply.github.com>
2022-11-07 08:33:28 -0300

    .

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -6,14 +6,6 @@ const subirImagenRegistro = require('../middlewares/imgRegistroMid');
 const validacionRegistro = require('../middlewares/validacionRegistroMid')
 
 
-const validacionRegistro = [
-    body('nombre').notEmpty().withMessage('Completar nombre'),
-    body('apellido').notEmpty().withMessage('Completar apellido'),
-    body('nombreUsuario').notEmpty().withMessage('Completar nombre de usuario'),
-    body('email')
-        .notEmpty().withMessage('Completar email').bail()   // el bail() detiene las validaciones siguientes en caso de que no se cumpla la que tiene el bail(). Se usa  para hacer mas de una validacion,  
-        .isEmail().withMessage('El formato del correo no es válido')  // en este caso si ya el campo esta vacio, no sigue con las validaciones siguientes, y si no esta vacio, valida que sea un mail (para este caso particular)
-];
 
 
 // Login

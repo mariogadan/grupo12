c385c029735c84331c427d15355e184fcc26ec7b
delfivilieri <110919394+delfivilieri@users.noreply.github.com>
2022-11-13 18:57:12 -0300

    Correr base de datos en alwaysdata y configuraciones

--- database/config/config.js
+++ database/config/config.js
@@ -1,23 +1,23 @@
 {
   "development": {
-    "username": "root",
-    "password": null,
+    "username": "288614_grupo12",
+    "password": "digitalhouse",
     "database": "database_development",
-    "host": "127.0.0.1",
+    "host": "mysql-delfinavilieri.alwaysdata.net",
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
+    "username": "288614_grupo12",
+    "password": "digitalhouse",
     "database": "database_test",
-    "host": "127.0.0.1",
+    "host": "mysql-delfinavilieri.alwaysdata.net",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
+    "username": "288614_grupo12",
+    "password": "digitalhouse",
     "database": "database_production",
-    "host": "127.0.0.1",
+    "host": "mysql-delfinavilieri.alwaysdata.net",
     "dialect": "mysql"
   }
 }

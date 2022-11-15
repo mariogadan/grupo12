911f48c0be3ce3b2b281a6a19149d51bfe45c862
Damian Palla <damian.palla@live.com>
2022-11-14 22:16:34 -0300

    .

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,21 +1,21 @@
 module.exports = {
   "development": {
     "username": "288614_grupo12",
-    "password": "digitalhouse",
+    "password": "digitalhouse12",
     "database": "database_development",
     "host": "mysql-delfinavilieri.alwaysdata.net",
     "dialect": "mysql"
   },
   "test": {
     "username": "288614_grupo12",
-    "password": "digitalhouse",
+    "password": "digitalhouse12",
     "database": "database_test",
     "host": "mysql-delfinavilieri.alwaysdata.net",
     "dialect": "mysql"
   },
   "production": {
     "username": "288614_grupo12",
-    "password": "digitalhouse",
+    "password": "digitalhouse12",
     "database": "database_production",
     "host": "mysql-delfinavilieri.alwaysdata.net",
     "dialect": "mysql"

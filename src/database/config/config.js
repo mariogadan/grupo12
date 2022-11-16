da127990150798c90d8c4825ff30505106944a75
OgrizovicFlavio <flavio.ogrizovic@gmail.com>
2022-11-16 20:52:45 -0300

    Tocando la configuración de Sequelize

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,21 +2,21 @@ module.exports = {
   "development": {
     "username": "288614_grupo12",
     "password": "digitalhouse12",
-    "database": "database_development",
+    "database": "delfinavilieri_grupo12nueva",
     "host": "mysql-delfinavilieri.alwaysdata.net",
     "dialect": "mysql"
   },
   "test": {
     "username": "288614_grupo12",
     "password": "digitalhouse12",
-    "database": "database_test",
+    "database": "delfinavilieri_grupo12nueva",
     "host": "mysql-delfinavilieri.alwaysdata.net",
     "dialect": "mysql"
   },
   "production": {
     "username": "288614_grupo12",
     "password": "digitalhouse12",
-    "database": "database_production",
+    "database": "delfinavilieri_grupo12nueva",
     "host": "mysql-delfinavilieri.alwaysdata.net",
     "dialect": "mysql"
   }

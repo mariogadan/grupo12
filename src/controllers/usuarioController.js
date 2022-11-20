230195b13aeb0ee9a41d16d18e975527fb3929bc
mariogadan <mario.gadan@gmail.com>
2022-11-20 18:25:37 -0300

    procesologin BD andando

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,126 +1,57 @@
-// const fs = require('fs') // VUELA
-// const path = require('path') // VUELA
+const { validationResult } = require('express-validator'); 
+const bcryptjs = require('bcryptjs'); 
 
-// const cursosFilePath = path.join(__dirname, '../database/cursosDataBase.json'); // VUELA
-// const cursos = JSON.parse(fs.readFileSync(cursosFilePath, 'utf-8')); // VUELA
-
-// const usuariosFilePath = path.join(__dirname, '../database/usuariosDataBase.json'); // VUELA
-// const usuarios = JSON.parse(fs.readFileSync(usuariosFilePath, 'utf-8')); //VUELA 
-
-const { validationResult } = require('express-validator'); // QUEDA
-const bcryptjs = require('bcryptjs'); // QUEDA
-
-let db = require("../database/models"); // NUEVO
-// const { create } = require('domain'); // ESTO APARECIÓ SOLO (??)
+let db = require("../database/models");
 
 const controlador = {
     
-    /*
-    login: function (req, res) {
-        res.render("login");
-    },
-    */
-     
-    // LOGIN - NUEVO - INICIO
+
     login: function (req, res) {
         db.usuario.findAll()
         .then(function(usuario){
             return res.render("login", {usuario:usuario})
         })
     },
-    // LOGIN - NUEVO - FIN 
-    
-    /*
-    registro: function (req, res) {
-        res.render("registro");
-    },
-    */
-    
-    // REGISTRO - NUEVO - INICIO
+
+
     registro: function (req, res) {
         db.usuario.findAll()
         .then(function(usuario){
             return res.render("registro", {usuario:usuario})
         })
     },
-    // REGISTRO - NUEVO - FIN 
-
 
     procesoLogin: function (req, res) {
         
         let errores = validationResult(req);
-        let usuarioALoguearse;
 
-        if (errores.isEmpty()) {
-            for (let i = 0; i < usuarios.length; i++) {
-                if ((req.body.email == usuarios[i].email && bcryptjs.compareSync(req.body.password, usuarios[i].password)))
-                {usuarioALoguearse = usuarios[i];
-                break};
-            }
-            if (usuarioALoguearse == undefined) {
-                return res.render("login", {
-                    errores: [{ msg: "Credenciales inválidas" }]
-                })
-
-            }
-            else {
-                req.session.usuarioLogueado = usuarioALoguearse;
-                res.redirect("/")
-            }
+        if (errores.isEmpty()){
+            db.usuario.findOne({ where : {email : req.body.email }})
+             .then(function(usuario){
+                if(usuario == null){
+                    return res.render("login", 
+                    {errores: [{ msg: "Usuario inexistente" }]});
+                } else {
+                let validaPass = bcryptjs.compare(req.body.password, usuario.clave)
+                    .then(function(validaPass){
+                        if(validaPass){
+                            req.session.usuarioLogueado = usuario;
+                            res.redirect("/");
+                        } else {
+                            return res.render("login", {
+                                errores: [{ msg: "Contraseña incorrecta" }]});
+                        }
+                    });
+                }
+            });
         } else {
             return res.render("login", {
                 errores: errores.array(),
                 old: req.body
             })
         }
-    },
-    
-    /*
-    //  este es el metodo procesoRegistro como lo teniamos antes - inicio
-    
-    procesoRegistro: function (req, res) {
+},
 
-        let errores = validationResult(req);
-
-        if (errores.isEmpty()) {
-
-            let usuario = req.body;
-            let idNuevoUsuario = (usuarios[usuarios.length - 1].id) + 1;
-
-            let avatar = "imagen vacia";
-
-            if (req.file) {
-                avatar = req.file.filename
-            };
-            let nuevoUsuario = {
-                "id": idNuevoUsuario,
-                "nombre": usuario.nombre,
-                "apellido": usuario.apellido,
-                "nombreUsuario": usuario.nombreUsuario,
-                "email": usuario.email,
-                "password": bcryptjs.hashSync(req.body.password, 10),
-                "fechaNacimiento": usuario.dia + "/" + usuario.mes + "/" + usuario.anio,
-                "avatar": avatar,
-
-            };
-
-            usuarios.push(nuevoUsuario);
-            fs.writeFileSync(usuariosFilePath, JSON.stringify(usuarios, null, " "), "utf-8");
-
-            res.redirect('/')
-        }
-        else {
-            res.render("registro", {
-                errores: errores.array(),
-                old: req.body
-            });
-        }
-    }
-    //  este es el metodo procesoRegistro como lo teniamos antes - fin   
-    */ 
-
-    // es es el metodo procesoRegistro nuev - inicio
-    
     procesoRegistro: function (req, res) {
 
          let errores = validationResult(req);
@@ -134,7 +65,7 @@ const controlador = {
             if (req.file) {
                 avatar = req.file.filename
             };
-            //console.log(req.body)
+
             db.usuario.create({
             nombre: req.body.nombre,
             apellido: req.body.apellido,
@@ -145,7 +76,6 @@ const controlador = {
             superadmin: 0, // req.body.superadmin,
             idAcademia: 1 // req.body.idAcademia
             });
-            console.log('ya sali de create()')
             res.redirect('/')
         }
         else {
@@ -155,7 +85,6 @@ const controlador = {
             });
         }
     }
-    // es es el metodo procesoRegistro nuev - fin
     
 };
 

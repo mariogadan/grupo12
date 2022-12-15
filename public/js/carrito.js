fae764de7c784e34bf8b4df185d916d7aae43596
Damian Palla <damian.palla@live.com>
2022-12-15 19:36:02 -0300

    .

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -1,41 +1,54 @@
 window.addEventListener('load', function () {
 
-    //Capturando los inputs//
+    //Capturando los inputs desde la vista de PRODUCTO//
 
     let botonCarrito = document.querySelector('.reserva')
+
+    let nombreCurso = document.querySelector('.titulo-bienvenida')
+
+    let imagenCurso = document.querySelector('.imagen-bienvenida')
+
+    let precioCurso = document.querySelector('.fecha-precio')
+
+    let idCurso = document.querySelector('.idCurso')
+
+
+    //Capturando los inputs desde la vista de CARRITO//
+
+    let cursoCaja = document.querySelector('.cursoCaja')
+
+    let cursoCarrito = document.querySelector('.cursoCarrito')
+
+    let imagenCursoCarrito = document.querySelector('.imagen-curso')
+
+    let nombreCursoCarrito = document.querySelector('.nombreCursoCarrito')
+
+    let precioCursoCarrito = document.querySelector('.precioCursoCarrito')
+
+   
+
+    let carrito = []
 
 
     botonCarrito.addEventListener('click', function (e) {
 
-        let carrito = []
-
-        
-        if( (e =! "") && (e == botonCarrito)){
-
-        let nombreCurso = document.querySelector('.titulo-bienvenida')
-
-        let imagenCurso = document.querySelector('.imagen-bienvenida')
-
-        let precioCurso = document.querySelector('.fecha-precio')
-
-        let idCurso = document.querySelector('.idCurso')
 
 
+        if (e.target.classList.contains('reserva')){
 
-        carrito.push({
-            id: idCurso.value,
-            nombre: nombreCurso.value,
-            img: imagenCurso.value,
-            precio: precioCurso.value
-        })
+            carrito.push({                          // toma la info de la vista producto y crea el objeto curso que se va a mostrar en la vista de carrito
+                id: idCurso.textContent,
+                nombre: nombreCurso.textContent,
+                img: imagenCurso.textContent,
+                precio: precioCurso.textContent
+            })
+            console.log(nombreCurso.textContent)
+        }
 
-        console.log(carrito)
-        
-    } 
-
-    })
+    }) 
 
 })
+
 
 /*
 

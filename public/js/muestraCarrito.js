1bc68a3d99ec229af8ea16f2922ae6cc79be0b14
OgrizovicFlavio <flavio.ogrizovic@gmail.com>
2022-12-16 17:59:43 -0300

    dsaf

---
+++ public/js/muestraCarrito.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function () {
+
+    //Capturando los inputs desde la vista de CARRITO//
+
+    let cursoCaja = document.getElementById('cursoCaja');
+
+    let cursoCarrito = document.getElementById('cursoCarrito');
+
+    let imagenCursoCarrito = document.getElementById('imagen-curso');
+
+    let nombreCursoCarrito = document.getElementById('nombreCursoCarrito');
+
+    let precioCursoCarrito = document.getElementById('precioCursoCarrito');
+
+
+
+
+})

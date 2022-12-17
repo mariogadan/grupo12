2e1e70094ab7f1b114cc8263e27e6eb5c80b3428
Damian Palla <damian.palla@live.com>
2022-12-16 21:58:05 -0300

    .

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -96,7 +96,7 @@ const controlador = {
                     descripcion: req.body.descripcion,
                     vacantesTotales: req.body.vacantesTotales,
                     idTipoCurso: req.body.tipoCurso,
-                    idAdmin: req.body.idAdmin, //Una vez que tengamos el controller de Usuarios conectado a la base de datos, deberíamos crear un middleware para verificar con Session que somos administradores y eso lo tome el formulario.
+                    idAdmin: 1 //req.body.idAdmin , Una vez que tengamos el controller de Usuarios conectado a la base de datos, deberíamos crear un middleware para verificar con Session que somos administradores y eso lo tome el formulario.
                 }, {
                     where: {
                         idCurso: req.params.id
